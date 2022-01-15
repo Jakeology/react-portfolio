e18ad1349bbaf66124685df7bd4a16b2c74abaf4
Jake <xxjakexx14@gmail.com>
2022-01-15 17:06:15 -0500

    added css syling to content titles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 function About() {
+
+  const title = "About";
+
   return (
     <div className="container-fluid">
       <div className="m-1 row d-flex justify-content-center mb-5">
         <div className="col-12 col-xxl-8">
           <div className="primary-section-title">
-            <h2>Home</h2>
+            <h2>{title}</h2>
+            <span>{title}</span>
           </div>
         </div>
       </div>
