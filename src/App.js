7b818a6ba7d8221e322949ae58b16e04e53fe8a9
Jake <xxjakexx14@gmail.com>
2022-01-14 21:55:48 -0500

    added Home component and component render

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
 import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
   const [categories] = useState([
-    { name: "Home"},
-    { name: "About"},
-    { name: "Portfolio"},
+    { name: "Home", child: Home },
+    { name: "About" },
+    { name: "Portfolio" },
     { name: "Resume" },
     { name: "Contact" },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const ActiveChild = currentCategory.child;
+
   return (
     <div>
       <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}></Nav>
+      <main>
+        <ActiveChild />
+      </main>
     </div>
   );
 }
