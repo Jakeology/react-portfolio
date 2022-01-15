91fe0f051011e32d24d9920718943903195e9b51
Jake <xxjakexx14@gmail.com>
2022-01-15 16:36:42 -0500

    added bootstrap and css styling to content titles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function About() {
   return (
-    <div className="test">
-      <h1>About</h1>
+    <div className="container-fluid">
+      <div className="m-1 row d-flex justify-content-center mb-5">
+        <div className="col-12 col-xxl-8">
+          <div className="primary-section-title">
+            <h2>Home</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
