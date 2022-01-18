29e7b35ace86ee4e5dc5d3f8387b7bcb9c4ea393
Jake <xxjakexx14@gmail.com>
2022-01-17 21:59:47 -0500

    changed portfolio cards

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Portfolio() {
-
   const title = "portfolio";
 
   return (
@@ -14,15 +13,15 @@ function Portfolio() {
       <div className="portfolio-primary">
         <div className="cards">
           <div className="portfolio-card">
-            <img className="card-image" src="/images/calculator.png" alt="Calculator" />
+            <img className="card-image" src="/images/run-buddy.png" alt="Run Buddy" />
             <div className="card-bio">
-              <h3>Calculator</h3>
-              <p>React/CSS/JavaScript</p>
+              <h3>Run Buddy</h3>
+              <p>HTML • CSS</p>
               <span>
-                <a href="/">
+                <a href="https://jakeology.github.io/run-buddy/" target="_blank" rel="noreferrer">
                   <i className="fa-light fa-link"></i>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Jakeology/run-buddy" target="_blank" rel="noreferrer">
                   <i className="fa-brands fa-github"></i>
                 </a>
               </span>
@@ -30,15 +29,15 @@ function Portfolio() {
           </div>
 
           <div className="portfolio-card">
-            <img className="card-image" src="/images/led-wall.jpg" alt="Led Wall" />
+            <img className="card-image" src="/images/scheduler.png" alt="Work Scheduler" />
             <div className="card-bio">
-              <h3>Led Wall</h3>
-              <p>Node.js/loT</p>
+              <h3>Work Scheduler</h3>
+              <p>HTML • CSS • JavaScript</p>
               <span>
-                <a href="/">
+                <a href="https://jakeology.github.io/work-scheduler/" target="_blank" rel="noreferrer">
                   <i className="fa-light fa-link"></i>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Jakeology/work-scheduler" target="_blank" rel="noreferrer">
                   <i className="fa-brands fa-github"></i>
                 </a>
               </span>
@@ -46,15 +45,15 @@ function Portfolio() {
           </div>
 
           <article className="portfolio-card">
-            <img className="card-image" src="/images/moutain.png" alt="Moutain" />
+            <img className="card-image" src="/images/safetravel.png" alt="Safe Travel" />
             <div className="card-bio">
-              <h3>Moutain</h3>
-              <p>Surf Report</p>
+              <h3>Safe Travel</h3>
+              <p>JavaScript • REST API</p>
               <span>
-                <a href="/">
+                <a href="https://jakeology.github.io/safe-travels/" target="_blank" rel="noreferrer">
                   <i className="fa-light fa-link"></i>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Jakeology/safe-travels" target="_blank" rel="noreferrer">
                   <i className="fa-brands fa-github"></i>
                 </a>
               </span>
@@ -62,15 +61,47 @@ function Portfolio() {
           </article>
 
           <article className="portfolio-card">
-            <img className="card-image" src="/images/puzzle.jpg" alt="Pretzel Puzzle" />
+            <img className="card-image" src="/images/tech-blog.png" alt="Tech Blog" />
             <div className="card-bio">
-              <h3>Pretzel Puzzle</h3>
-              <p>MERN Stack</p>
+              <h3>Tech Blog</h3>
+              <p>NodeJS • Sequelize • HandlebarsJS</p>
               <span>
-                <a href="/">
+                <a href="https://lit-coast-25715.herokuapp.com/" target="_blank" rel="noreferrer">
                   <i className="fa-light fa-link"></i>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Jakeology/tech-blog" target="_blank" rel="noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </span>
+            </div>
+          </article>
+
+          <article className="portfolio-card">
+            <img className="card-image" src="/images/paws-n-play.png" alt="Paws n Play" />
+            <div className="card-bio">
+              <h3>Tech Blog</h3>
+              <p>ExpressJS • Sequelize • HandlebarsJS</p>
+              <span>
+                <a href="https://calm-scrubland-16767.herokuapp.com/" target="_blank" rel="noreferrer">
+                  <i className="fa-light fa-link"></i>
+                </a>
+                <a href="https://github.com/Jakeology/paws-n-play" target="_blank" rel="noreferrer">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </span>
+            </div>
+          </article>
+
+          <article className="portfolio-card">
+            <img className="card-image" src="/images/budget-tracker.png" alt="Budget Tracker" />
+            <div className="card-bio">
+              <h3>Budget Tracker</h3>
+              <p>ExpressJS • Mongoose • MongoDB</p>
+              <span>
+                <a href="https://thawing-mesa-44715.herokuapp.com/" target="_blank" rel="noreferrer">
+                  <i className="fa-light fa-link"></i>
+                </a>
+                <a href="https://github.com/Jakeology/budget-tracker" target="_blank" rel="noreferrer">
                   <i className="fa-brands fa-github"></i>
                 </a>
               </span>
