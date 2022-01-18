d752291f0fe5964d1a4fabfa58129ff277b1730f
Jake <xxjakexx14@gmail.com>
2022-01-17 22:14:12 -0500

    added user links to nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,10 +7,20 @@ function Nav(props) {
     <header>
       <nav className="header-container">
         <div className="header-image">
-          <a href="/">
+          <a className="image-link" href="/">
             <img alt="jake" src="/images/pfp.jpeg"></img>
           </a>
-          <h2>Jacob Bartoletta</h2>
+          <div className="header-links">
+            <a href="https://github.com/Jakeology" target="_blank" rel="noreferrer">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/jacob-bartoletta-9b1566216/" target="_blank" rel="noreferrer">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://stackoverflow.com/users/16644863/jacob-b" target="_blank" rel="noreferrer">
+              <i class="fa-brands fa-stack-overflow"></i>
+            </a>
+          </div>
         </div>
         <ul className="header-menu">
           {categories.map((category) => (
